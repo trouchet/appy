126bea58b70f8f7ca7fa9a4726b5fd2e30ecba5a
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-21 17:38:16 -0300

    refac/ app->app_y

--- src/routes/users.py
+++ src/routes/users.py
@@ -6,7 +6,7 @@ sys.path.append('../')
 from appy import app
 
 
-@app_y.route('/users/<username>')
+@app.route('/users/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return f'User {escape(username)}'
