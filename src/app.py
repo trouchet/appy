e77de3211cae06a59bce81998bf5a705c6b1277f
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-27 16:01:07 -0300

    refac/ create_app

--- src/app.py
+++ src/app.py
@@ -1,9 +1,26 @@
 from __future__ import annotations
 
-from flask import Flask
+from os import path
+from flask import Flask, send_from_directory
 
-from blueprints.bundler import blueprints
+# App Factory: https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
+def create_app():
+    app = Flask(__name__)
+    # app.config.from_object(config_filename) 
 
-app = Flask(__name__)
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(
+            path.join(app.root_path, 'static'),
+            'favicon.ico'
+    )
+    
+    from .blueprints.bundler import blueprints
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
 
-map(lambda blueprint: app.register_blueprint(blueprint), blueprints)
+    return app
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from app import app
+from .app import create_app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run()
