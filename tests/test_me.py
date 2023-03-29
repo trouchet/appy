137be7a8c6655fc03aed7a5f7a4c49cd0ab15c13
Bruno Peixoto <brunolnetto@gmail.com>
2023-03-29 17:34:38 -0300

    docs/ add references to application test

--- tests/test_me.py
+++ tests/test_me.py
@@ -1,6 +1,8 @@
 # test_me.py
 
-# Take a look at: https://testdriven.io/blog/flask-pytest/
+# Take a look at: 
+#   1. https://testdriven.io/blog/flask-pytest/
+#   2. https://flask.palletsprojects.com/en/1.1.x/testing/
 
 from __future__ import annotations
 
